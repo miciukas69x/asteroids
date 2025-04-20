1378033c48cc1f8b40ef00907c3cf44a5899470b
miciukas69x <michailinasmatas@gmail.com>
2025-04-20 15:07:27 +0200

    Made the player

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import pygame
 from constants import *
+from player import Player
+from circleshape import CircleShape
 
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+    Player.containers = (updatable, drawable)
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-                
-        screen.fill("black")
-        pygame.display.flip()
-
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        dt = game_clock.tick(60) / 1000  # seconds
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        updatable.update(dt)
+        screen.fill("black")
+        for entity in drawable:
+            entity.draw(screen)
+        pygame.display.flip()
+
+    
 if __name__ == "__main__":
     main()
