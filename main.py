56477aeae6d78d3a71a3024ace1d23adaf56ff47
miciukas69x <michailinasmatas@gmail.com>
2025-04-20 21:48:44 +0200

    created the rough version of asteroids

--- main.py
+++ main.py
@@ -2,18 +2,27 @@ import pygame
 from constants import *
 from player import Player
 from circleshape import CircleShape
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 
+asteroids = pygame.sprite.Group()
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
     dt = 0
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
+    asteroid_field = AsteroidField()
     
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -23,6 +32,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
 
         updatable.update(dt)
         screen.fill("black")
