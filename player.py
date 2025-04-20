0e3371febed0cdc0551fa14ff91af1ec8711bcd7
miciukas69x <michailinasmatas@gmail.com>
2025-04-20 22:08:56 +0200

    fixed smth

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import *
 
 
 class Player(CircleShape):
@@ -8,6 +8,8 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
     # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
